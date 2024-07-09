c67a28368cf67236d8ac00c9cf225735805a2d1b
Justin-Akridge <akridjus@gmail.com>
2024-07-09 16:51:28 -0400

    file uploader

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
   "encoding/json"
   "html/template"
+  "io/ioutil"
 	"fmt"
 	"net/http"
   "time"
@@ -413,6 +414,37 @@ func insertJobIntoDb(jobName, companyName, adminId string, db *sql.DB) (string, 
 
 func HandleUploadLas(store *sessions.CookieStore, db *sql.DB) http.HandlerFunc {
   return func(w http.ResponseWriter, r *http.Request) {
-    http.Redirect(w, r, fmt.Sprintf("/map"), http.StatusSeeOther)
+    func ReceiveFile(w http.ResponseWriter, r *http.Request) {
+    r.ParseMultipartForm(32 << 20) // limit your max input length!
+    var buf bytes.Buffer
+    // in your case file would be fileupload
+    file, header, err := r.FormFile("file")
+    if err != nil {
+        panic(err)
+    }
+    defer file.Close()
+    name := strings.Split(header.Filename, ".")
+    fmt.Printf("File name %s\n", name[0])
+    // Copy the file data to my buffer
+    io.Copy(&buf, file)
+    // do something with the contents...
+    // I normally have a struct defined and unmarshal into a struct, but this will
+    // work as an example
+    contents := buf.String()
+    fmt.Println(contents)
+    // I reset the buffer in case I want to use it again
+    // reduces memory allocations in more intense projects
+    buf.Reset()
+    // do something else
+    // etc write header
+    return
   }
+}
+
+func isLasFile(data []byte) bool {
+  const lasHeader = ".lasf"
+  if len(data) < len(lasHeader) {
+    return false
+  }
+  return string(data[:len(lasHeader)]) == lasHeader
 }
